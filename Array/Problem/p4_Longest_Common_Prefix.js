862afc121cb4bcb26a36db6086d869fe5a59ffda
rahulchaurasiya2981-droid <rahul.chaurasiya2981@gmail.com>
2026-04-23 00:41:26 +0530

    P4 : solved with 3 method (prefix shrink,build prefix and sorting)

--- Array/Problem/p4_Longest_Common_Prefix.js
+++ Array/Problem/p4_Longest_Common_Prefix.js
@@ -11,28 +11,76 @@
 // Explanation: There is no common prefix among the input strings.
 
 // ---------------------------------------------------------------------------->>>
-// M1 : Brute Force (Build prefix)
-// TC : O(n * m²)
+// M1 : Brute Force (Shrink prefix)(Horizontal scaling)(better than M2,M3)
+// TC : O(n * m)  
+// - n = number of strings
+// - m = length of the shortest string (or prefix)
 // SC : O(1)
 
+// const longestCommonPrefix = (strs) => {
+//     if(!strs.length || strs === null) return '';
+//     let prefix=strs[0] || ""; // assume first string is common prefix
 
-// M2 : Brute Force (Shrink prefix)
+//     for (let i=1;i<strs.length;i++) // loop the remaining array string item
+//     {
+//         let j=0;
+//         while(j<prefix.length && j<strs[i].length && prefix[j]===strs[i][j]) j++;
+//         if(j<prefix.length) prefix = prefix.slice(0,j); // slice if needs
+//         if(!prefix.length) return "";
+//     }
+//     return prefix;
+// };
+
+
+// ------------------------------------------------------------------------------->>>>
+// M2 : Brute Force (Build prefix)(Horizontal scaling)
 // TC : O(n * m)
 // SC : O(1)
-const longestCommonPrefix = (strs) => {
-    let prefix=strs[0]; // assume first string is common prefix
 
-    for (let i=1;i<strs.length;i++) // loop the remaining array string item
-    {
-        for(let j=0;j<prefix.length;j++) // loop the assumed common prefix
-        {
-            if(prefix[j] == strs[i][j]) continue;
-            else{
-                prefix = prefix.slice(0,j);// also give empty string
-                break;
-            };  
-        }
-    }
-    return prefix;
-};
+// const longestCommonPrefix = (strs) => {
+//         if(!strs.length || strs === null) return '';
+//     let prefix="";
+//     for(let i=0;i<strs[0].length;i++)
+//     {
+//         const char=strs[0][i];
+//         for(let j=1;j<strs.length;j++)
+//         {
+//             if(i>=strs[j].length || strs[j][i] !== char) return prefix;
+//         }
+//         prefix+=char;
+//     }
+//     return prefix;
+// }
+
+
+// ------------------------------------------------------------------------------->>>>
+// M3 : Using sorting
+// TC : O((n log n) * m)
+// - n = number of strings
+// - m = length of shortest string
+// n log n → sorting strings
+// m → cost of string comparison (character by character)
+
+// SC : O(m)   
+// - prefix can grow up to length M
+
+// - Time complexity is O(N log N * M) due to sorting with string comparisons, and space complexity is O(M) for the prefix.
+
+// const longestCommonPrefix = (strs) => {
+//         // base case 
+//     if(!strs.length || strs === null) return "";
+//     strs.sort();
+//     let first = strs[0];
+//     let last = strs[strs.length-1];
+//     let prefix='';
+//     for(let i=0;i<first.length;i++)
+//     {
+//         if(first[i] !== last[i]) break;
+//         prefix+=first[i];
+//     }
+//     return prefix;
+// }
 console.log(longestCommonPrefix(["flow", "flower", "flight"])); 
+
+// ------------------------------------------------------------------------------->>>>
+// M4 : Using TRIE (need to learn bust classes and concept)
